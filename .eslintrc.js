3eeb7c20f3e371f3a1e576bbc26c2798525fb992
claurier <carl.laurier@sword-group.com>
2018-10-08 16:57:39 +0200

    Fusion entre le POC envoyé par Algolia et le boilerplate d Algolia Affichage correct des données Reste à faire : CSS entre autres

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: 'algolia/react',
   rules: {
-  "linebreak-style":["error", "windows"]
-}
+    "linebreak-style": ["error", "windows"],
+    "react/prop-types": [
+      "enabled",
+      {"ignore": "ignore", "customValidators": "customValidator"}
+    ]
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './containers/App';
+import app from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(app);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="*" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
