707ed5f9173fb7e9a9fdb013d5eb837bf46a61db
claurier <carl.laurier@sword-group.com>
2018-10-19 17:19:25 +0200

    CSS for desktop / #ContainerSortBy

--- src/containers/App.js
+++ src/containers/App.js
@@ -124,7 +124,7 @@ class App extends Component {
                     onSearchStateChange={this.handleSearchState}
                 >
                     <div id="container-main-searchbox" className="Grid">
-                        <div className="Grid-cell u-1of5">
+                        <div className="Grid-cell">
                             <SearchBox className="fake-column"
                                    translations={{
                                        placeholder: '',
